e06e266ae8b0fad108bfdc1e0d01b5116d2a58bc
dharmabm <keving@rowantree-oss.com>
2013-12-16 13:33:55 +0700

    changed device makefiles from cm to slim

--- slim.mk
+++ slim.mk
@@ -1,18 +1,18 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/p970/p970.mk)
 
-PRODUCT_NAME := cm_p970
+PRODUCT_NAME := slim_p970
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusBlack
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
--include vendor/cm/config/common_versions.mk
+-include vendor/slim/config/common_versions.mk
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p970
